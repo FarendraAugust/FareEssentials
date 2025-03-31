d7a25a1c3dce81058962fabd0c1af1d188f7e73c
FarendraAugust <fariskhaagustinofarendra@gmail.com>
2025-03-31 21:24:32 +0700

    Main Plugins

---
+++ src/me/Farendra/FareEssentials/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace me\Farendra\FareEssentials\Main;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+  private Config $configFile;
+
+  public function onEnable(): void
+  {
+    $this->saveDefaultConfig();
+    $this->configFile = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    
+    $this->getLogger()->info("&aFareEssentials are enable!");
+  }
+  
+  public function onDisable(): void {
+    $this->getLogger()->info("&cFareEssentials are disable!");
+  }
+}
